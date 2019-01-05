0d257243415905e4ba96854b0d497859ac82da6e
yinm <yinm.cminor9@gmail.com>
2019-01-06 08:09:31 +0900

    Check 2nd

--- getting_started/mock.test.js
+++ getting_started/mock.test.js
@@ -1,9 +1,15 @@
-test('check mock', () => {
-  const myMockFn = jest
-    .fn()
-    .mockReturnValue('default')
-    .mockImplementation(scalar => 42 + scalar)
-    .mockName('add42')
+function forEach(items, callback) {
+  for (let i = 0; i < items.length; i++) {
+    callback(items[i])
+  }
+}
 
-  expect(myMockFn(10)).toBe(52)
+test('check mock', () => {
+  const mockCallback = jest.fn(x => 42 + x)
+  forEach([0, 1], mockCallback)
+
+  expect(mockCallback.mock.calls.length).toBe(2)
+  expect(mockCallback.mock.calls[0][0]).toBe(0)
+  expect(mockCallback.mock.calls[1][0]).toBe(1)
+  expect(mockCallback.mock.results[0].value).toBe(42)
 })
