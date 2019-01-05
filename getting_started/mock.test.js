9884247801fc58d729255d5d853b0cf6e5cf33b7
yinm <yinm.cminor9@gmail.com>
2019-01-05 11:04:12 +0900

    Check .mock property

--- getting_started/mock.test.js
+++ getting_started/mock.test.js
@@ -1,15 +1,11 @@
-function forEach(items, callback) {
-  for (let index = 0; index < items.length; index++) {
-    callback(items[index])
-  }
-}
+test('check mock', () => {
+  const myMock = jest.fn()
 
-test('check mocks', () => {
-  const mockCallback = jest.fn(x => 42 + x)
-  forEach([0, 1], mockCallback)
+  const a = new myMock()
+  const b = {}
+  const bound = myMock.bind(b)
+  bound()
 
-  expect(mockCallback.mock.calls.length).toBe(2)
-  expect(mockCallback.mock.calls[0][0]).toBe(0)
-  expect(mockCallback.mock.calls[1][0]).toBe(1)
-  expect(mockCallback.mock.results[0].value).toBe(42)
+  console.log(myMock.mock.instances)
 })
+
