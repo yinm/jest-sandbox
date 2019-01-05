c22ff7f9647fdc34215b7c4e4f3ce343d5fe8e6e
yinm <yinm.cminor9@gmail.com>
2019-01-05 12:29:28 +0900

    Check mockImplementationOnce()

--- getting_started/mock.test.js
+++ getting_started/mock.test.js
@@ -1,7 +1,15 @@
 test('check mock', () => {
-  const myMockFn = jest.fn(cb => cb(null, true))
+  const myMockFn = jest
+    .fn()
+    .mockImplementationOnce(cb => cb(null, true))
+    .mockImplementationOnce(cb => cb(null, false))
 
   myMockFn((err, val) => console.log(val))
-
   myMockFn((err, val) => console.log(val))
 })
+
+function noname(cb) {
+  cb(null, true)
+}
+noname((err, val) => console.log(val))
+
