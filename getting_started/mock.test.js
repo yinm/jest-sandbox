3da0cbbf5b05f422139113849de87ee1bd67bb10
yinm <yinm.cminor9@gmail.com>
2019-01-06 08:28:37 +0900

    Check 2nd

--- getting_started/mock.test.js
+++ getting_started/mock.test.js
@@ -1,9 +1,14 @@
-test('return values', () => {
-  const filterTestFn = jest.fn()
+test('mock implementation', () => {
+  const myMockFn = jest.fn(cb => cb(null, true))
 
-  filterTestFn.mockReturnValueOnce(true).mockReturnValueOnce(false)
-  const result = [11, 12].filter(filterTestFn)
-
-  console.log(result)
-  console.log(filterTestFn.mock.calls)
+  myMockFn((err, val) => console.log(val))
+  myMockFn((err, val) => console.log(val))
 })
+
+function noname(cb) {
+  cb(null, true)
+}
+noname((err, val) => console.log(val))
+
+const noname2 = cb => cb(null, true)
+noname2((err, val) => console.log(val))
