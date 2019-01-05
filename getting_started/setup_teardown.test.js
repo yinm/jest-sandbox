d479796af41e9ec5543ec8159422853828474776
yinm <yinm.cminor9@gmail.com>
2019-01-05 10:32:36 +0900

    Check scoping

--- getting_started/setup_teardown.test.js
+++ getting_started/setup_teardown.test.js
@@ -1,38 +1,13 @@
-let database
+beforeAll(() => console.log('1 - beforeAll'))
+afterAll(() => console.log('1 - afterAll'))
+beforeEach(() => console.log('1 - beforeEach'))
+afterEach(() => console.log('1 - afterEach'))
+test('', () => console.log('1 - test'))
 
-function initializeCityDatabase() {
-  return new Promise((resolve, reject) => {
-    database = [
-      'Vienna',
-      'San Juan'
-    ]
-    resolve()
-  })
-}
-
-function clearCityDatabase() {
-  return new Promise((resolve, reject) => {
-    database = []
-    resolve()
-  })
-}
-
-function isCity(city) {
-  return database.includes(city)
-}
-
-beforeAll(() => {
-  return initializeCityDatabase()
-})
-
-afterAll(() => {
-  return clearCityDatabase()
-})
-
-test('city database has Vienna', () => {
-  expect(isCity('Vienna')).toBeTruthy()
-})
-
-test('city database does not have Tokyo', () => {
-  expect(isCity('Tokyo')).toBeFalsy()
+describe('Scoped / Nested block', () => {
+  beforeAll(() => console.log('2 - beforeAll'))
+  afterAll(() => console.log('2 - afterAll'))
+  beforeEach(() => console.log('2 - beforeEach'))
+  afterEach(() => console.log('2 - afterEach'))
+  test('', () => console.log('2 - test'))
 })
