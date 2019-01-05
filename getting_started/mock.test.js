0ae7020660930d9dcf8395a7259753c8a453ba71
yinm <yinm.cminor9@gmail.com>
2019-01-06 08:17:42 +0900

    Check 2nd

--- getting_started/mock.test.js
+++ getting_started/mock.test.js
@@ -1,15 +1,11 @@
-function forEach(items, callback) {
-  for (let i = 0; i < items.length; i++) {
-    callback(items[i])
-  }
-}
+test('return values', () => {
+  const myMock = jest.fn()
+  console.log(myMock())
 
-test('check mock', () => {
-  const mockCallback = jest.fn(x => 42 + x)
-  forEach([0, 1], mockCallback)
+  myMock
+    .mockReturnValueOnce(10)
+    .mockReturnValueOnce('x')
+    .mockReturnValue(true)
 
-  expect(mockCallback.mock.calls.length).toBe(2)
-  expect(mockCallback.mock.calls[0][0]).toBe(0)
-  expect(mockCallback.mock.calls[1][0]).toBe(1)
-  expect(mockCallback.mock.results[0].value).toBe(42)
+  console.log(myMock(), myMock(), myMock(), myMock())
 })
